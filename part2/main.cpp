feeaf84522d1af877961fc9f90ef016bfa82608f
David Chen <yu_de_chen@yahoo.com>
2017-11-27 14:30:17 -0600

    _

--- part2/main.cpp
+++ part2/main.cpp
@@ -1,11 +1,13 @@
 #include "includes.h"
 #include "Classifier.h"
 
-void classify(const char *TEST_FILENAME, Classifier &classifier) {
+void classify(const char *TEST_FILENAME, Classifier &classifier, int &yes, int &no) {
 
     char buf[W + 10];
     int x = 0, y = 0;
-    int yes = 0, no = 0;
+    // int yes = 0, no = 0;
+
+    yes = no = 0;
 
     FILE *test = fopen(TEST_FILENAME, "r");
 
@@ -42,27 +44,43 @@ void classify(const char *TEST_FILENAME, Classifier &classifier) {
 
     }
 
-    printf("yes: %d\n", yes);
-    printf("no : %d\n\n", no);
+    // printf("yes: %d\n", yes);
+    // printf("no : %d\n\n", no);
+    // printf("%d\t%d\n", yes, no);
 
     fclose(test);
 
 }
 
-int main() {
+double test(const double K) {
 
     Classifier classifier;
+    int yes, no;
+    double accuracy = 0;
 
     classifier.train(YES_TRAIN, Class::YES);
     classifier.train(NO_TRAIN, Class::NO);
-    classifier.doneTraining();
+    classifier.doneTraining(K);
 
-    puts(YES_TEST);
-    classify(YES_TEST, classifier);
+    // puts(YES_TEST);
+    // printf("%f\t%s\t", K, YES_TEST);
+    classify(YES_TEST, classifier, yes, no);
+    accuracy += (double) yes / (yes + no);
 
-    puts(NO_TEST);
-    classify(NO_TEST, classifier);
+    // puts(NO_TEST);
+    // printf("%f\t%s\t", K, NO_TEST);
+    classify(NO_TEST, classifier, yes, no);
+    accuracy += (double) no / (yes + no);
 
+    accuracy = accuracy / 2.0 * 100;
+
+    return accuracy;
+
+}
+
+int main() {
+    for (double k = 0.1; k <= 5; k += 0.1) {
+        printf("%f\t%f\n", k, test((const double) k));
+    }
     return 0;
-
 }
